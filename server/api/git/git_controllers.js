20696357518ccf89c1e53d8a831cf46fdda614e2
Ken Finnigan <ken@kenfinnigan.me>
2015-04-09 14:44:17 -0400

    Create hierarchy of files and folders necessary for treeview component. Resolves #5 - Takes the current git index, then applies deletes and new files from the repository status to get list of current file paths - Converts a flat file path into a hierarchy of nodes representing folders and files

--- server/api/git/git_controllers.js
+++ server/api/git/git_controllers.js
@@ -65,31 +65,100 @@ module.exports = exports = {
   getFiles: function(req, res, next) {
     fs.exists(repoPath, function(exists) {
       if (exists) {
-        var filename = req.params.filename + req.params[0];
         var paths = [];
         nodegit.Repository.open(path.resolve(repoPath, '.git')).then(function(repo) {
-          return repo.getMasterCommit();
-        })
-        .then(function(commit) {
-          return commit.getTree();
-        })
-        .then(function(tree) {
-          // `walk()` returns an event.
-          var walker = tree.walk();
-          walker.on('entry', function(entry) {
-            var path = entry.path();
-            // console.log(path);
-            paths.push(path)
+          return repo.index().then(function(index) {
+            var flatIndex = index.entries().reduce(function(prev, entry) {
+              prev.push(entry.path);
+              return prev;
+            }, []);
+
+            return repo.getStatus().then(function(statuses) {
+              statuses.forEach(function(status) {
+                if (status.isNew()) {
+                  if (!flatIndex.some(function(element) { return element == status.path() })) {
+                    flatIndex.push(status.path());
+                  }
+                } else if (status.isDeleted()) {
+                  flatIndex = flatIndex.filter(function(element) {
+                    if (element == status.path()) {
+                      return false;
+                    }
+                    return true;
+                  });
+                }
+              });
+
+              function comparePaths(a, b) {
+                var aSlash = a.indexOf('/');
+                var bSlash = b.indexOf('/');
+
+                if (aSlash > 0) {
+                  // a has a directory path
+                  if (bSlash > 0) {
+                    // b has a directory path
+                    var firstComp = a.substring(0, aSlash).localeCompare(b.substring(0, bSlash));
+                    if (firstComp == 0) {
+                      // First directory path is equal
+                      return comparePaths(a.substring(aSlash + 1), b.substring(bSlash + 1));
+                    } else {
+                      // Unequal first directory path
+                      return firstComp;
+                    }
+                  } else {
+                    return -1;
+                  }
+                } else {
+                  // a is a file
+                  if (bSlash > 0) {
+                    // b has a directory path
+                    return 1;
+                  } else {
+                    // b is a file
+                    return a.localeCompare(b);
+                  }
+                }
+              }
+
+              flatIndex.sort(comparePaths);
+
+              function buildTree(element, fullPath, parentNode) {
+                var parts = element.split("/");
+                if (parts.length == 1) {
+                  // Handle file
+                  parentNode.push({
+                    text: parts[0],
+                    href: 'file?path=' + encodeURIComponent(fullPath),
+                    icon: 'fa fa-file-o'
+                  });
+                } else {
+                  // Handle directory
+                  var childNode = parentNode;
+                  for (var i=0; i < parentNode.length; i++) {
+                    if (parentNode[i].text == parts[0]) {
+                      childNode = parentNode[i];
+                      break;
+                    }
+                  }
+
+                  // Need to create child node
+                  if (childNode == parentNode) {
+                    childNode = {
+                      text: parts[0],
+                      nodes: []
+                    };
+                    parentNode.push(childNode);
+                  }
+
+                  buildTree(element.substring(parts[0].length + 1), fullPath, childNode.nodes);
+                }
+              }
+
+              flatIndex.forEach(function(element) { buildTree(element, element, paths); });
+
+              res.send(paths);
+            });
           });
-          walker.on('tree', function(entry) {
-            var path = entry.path();
-            // console.log(path);
-            paths.push(path)
-          });
-          walker.on('end', function(entries) {
-            res.send(paths);
-          });
-          walker.start();
         })
         .done()
       } else {

51052808c2354fd7965b9074672580d94080ff0d
Brian Leathem <bleathem@gmail.com>
2015-04-16 12:08:54 -0700

    Re-factored the git.spec.js to use solely the promise API instead of the "done" callback

--- server/api/git/git.spec.js
+++ server/api/git/git.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var request = require('supertest')
+var request = require("supertest-as-promised")
   , app = require('../../main/app')
   , _ = require('underscore')
   ;
@@ -10,34 +10,32 @@ require('should');
 describe('Rest git API:', function () {
   describe('POST /api/git/clone', function () {
     this.timeout(10000);
-    it('clone a repo', function (done) {
-      request(app).post('/api/git/clone')
+    it('clone a repo', function () {
+      return request(app).post('/api/git/clone')
         .send({repoUrl: 'https://github.com/bleathem/codeeditor.git'})
         .expect(200)
-        .end(function (err, res) {
+        .then(function (res) {
           // console.log(res.body);
           res.body.length.should.be.greaterThan(2);
-          done();
         });
     });
   });
   describe('GET /api/git/files', function () {
-    it('get a file listing', function (done) {
-      request(app).get('/api/git/files')
+    it('get a file listing', function () {
+      return request(app).get('/api/git/files')
         .expect(200)
-        .end(function (err, res) {
+        .then(function (res) {
           // console.log(res.body);
           res.body.length.should.be.greaterThan(2);
-          done();
         });
     });
   });
   describe('POST /api/git/file/diff/:filename', function () {
-    it('diff a file', function (done) {
-      request(app).post('/api/git/file/diff/gulpfile.js')
+    it('diff a file', function () {
+      return request(app).post('/api/git/file/diff/gulpfile.js')
         .send({text: ''})
         .expect(200)
-        .end(function (err, res) {
+        .then(function (res) {
           // console.log('*****\n', res.body, '\n*****');
           var diffs = res.body;
           diffs.length.should.equal(1);
@@ -45,17 +43,15 @@ describe('Rest git API:', function () {
           diffs[0].oldLines.should.be.greaterThan(2);
           diffs[0].newStart.should.equal(0);
           diffs[0].newLines.should.equal(0);
-          done();
         });
     });
   });
   describe('DELETE /api/git', function () {
-    it('delete the clone', function (done) {
-      request(app).delete('/api/git')
+    it('delete the clone', function () {
+      return request(app).delete('/api/git')
         .expect(200)
-        .end(function (err, res) {
+        .then(function (res) {
           res.body.exists.should.be.false;
-          done();
         });
     });
   });
